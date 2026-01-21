83ed8db599ac3b9a849c9a922ca8f3a7c54cad6e
Daniel Grayvold <85690571+dgrayvold@users.noreply.github.com>
2026-01-20 20:01:58 -0600

    Add Rolling Hills experiment

---
+++ src/components/experiments/RollingHills.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<canvas
+			v-for="i in 10"
+			:ref="el => blavas.push(el)"
+			class="absolute left-0 bottom-0 top-1/4 w-full h-full"
+		/>
+	</div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { Blava } from 'blava';
+
+defineOptions({
+	name: 'Rolling Hills',
+	creator: 'Daniel Grayvold',
+});
+
+const blavas = ref([]);
+
+onMounted(() => {
+	blavas.value.forEach((canvas, index) => {
+		new Blava(canvas, {
+			style: 'wave',
+			seed: `waveform-${index}`,
+			variance: {
+				x: 0,
+				y: 10,
+			},
+			gradient: `hsla(90, 81.82%, 43.14%, ${(index * 10) / 100})`,
+			movementSpeed: 'slow',
+		});
+	});
+});
+</script>
